37b181b8acf82670b0d00f7ea2d478c249949288
Ari <ari89.web@gmail.com>
2023-08-03 20:01:08 +0900

    Edit Index

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Appprofile from "./Appprofile";
-import AppCounter from "./AppCounter";
-import AppMovie from "./AppMovie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
